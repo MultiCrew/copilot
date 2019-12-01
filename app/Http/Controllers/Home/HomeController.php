d825702f486d21fea34977685cf3280a0680735f
Calum Shepherd <calum.shep@icloud.com>
2019-12-01 22:19:24 +0000

    added auth check to dashboard

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -14,6 +15,9 @@ class HomeController extends Controller
 
     public function dashboard()
     {
-        return view('home');
+        if (Auth::check())
+            return view('home');
+        else
+            return route('login');
     }
 }
