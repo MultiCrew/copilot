6a2970a6d48d9f59d630f3523ddab10dedb387c1
Calum Shepherd <calum.shep@icloud.com>
2019-12-01 22:22:34 +0000

    adjusted base layout

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,54 +1,101 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>MultiCrew</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <script src='https://www.google.com/recaptcha/api.js'></script>
-    </head>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
 
-    <body>
-        @if(!Cookie::get('cookie_consent'))
-            <div class="has-background-success has-text-white">
-                <div class="container">
-                    <div class="level" style="padding-top: 12px; padding-bottom: 12px;">
-                        <div class="level-left">
-                            <p class="">
-                                MultiCrew uses cookies to ensure you get the best experience. By continuing to browse, you agree to the storage of cookies on your computer.
-                            </p>
-                        </div>
-                        <div class="level-right">
-                            <a href="{{ route('cookie-consent')}}"><i class="fas fa-times has-text-white"></i></a>
-                        </div>
-                    </div>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
                 </div>
             </div>
+        </nav>
+
+        <main class="py-4 container">
+            @yield('content')
+        </main>
+        @if(!Cookie::get('cookie_consent'))
+            <div class="alert alert-warning alert-dismissible fade show border" role="alert"
+            style="position: fixed; z-index: 1000; bottom: 10px; text-align: center; width: 92%; left: 4%;">
+                MultiCrew uses cookies to ensure you get the best experience. By continuing to browse, you agree to the storage of cookies on your computer.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <a href="{{ route('cookie-consent')}}"><i class="fas fa-times has-text-white"></i></a>
+                </button>
+            </div>
         @endif
+    </div>
 
-        @yield('content')
+    <script src="{{ asset('js/app.js') }}"></script>
 
-        <script src="{{ asset('js/app.js') }}"></script>
-
-        @auth
-            <script>
-                function logout() {
-                    $("#logout-form").submit();
-                }
-            </script>
-        @endauth
+    @auth
         <script>
-            function toggleBurger() {
-                var burger = $('.burger');
-                var menu = $('.navbar-menu');
-                burger.toggleClass('is-active');
-                menu.toggleClass('is-active');
+            function logout() {
+                $("#logout-form").submit();
             }
         </script>
-        @yield('footer')
-    </body>
+    @endauth
+    <script>
+        function toggleBurger() {
+            var burger = $('.burger');
+            var menu = $('.navbar-menu');
+            burger.toggleClass('is-active');
+            menu.toggleClass('is-active');
+        }
+    </script>
+    @yield('footer')
+</body>
 </html>
