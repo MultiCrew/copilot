9a1470ee97ceedf49af45a8c1c05f6b7a8e41ffc
Calum Shepherd <calum.shep@icloud.com>
2019-12-13 01:30:15 +0000

    added some more sidebar icons

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -21,13 +21,13 @@
             <!-- Submenu content -->
             <div id='submenu1' class="collapse sidebar-submenu">
                 <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">All</span>
+                    <span class="menu-collapsed"><i class="fas fa-table fa-fw mr-2"></i>All</span>
                 </a>
                 <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">My Filters</span>
+                    <span class="menu-collapsed"><i class="fas fa-filter fa-fw mr-2"></i>My Filters</span>
                 </a>
                 <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">New Filter</span>
+                    <span class="menu-collapsed"><i class="fas fa-plus fa-fw mr-2"></i>New Filter</span>
                 </a>
             </div>
             <a href="#submenu2" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
@@ -40,10 +40,10 @@
             <!-- Submenu content -->
             <div id='submenu2' class="collapse sidebar-submenu">
                 <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Open Requests</span>
+                    <span class="menu-collapsed"><i class="fas fa-folder-open fa-fw mr-2"></i>Open Requests</span>
                 </a>
                 <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">New Request</span>
+                    <span class="menu-collapsed"><i class="fas fa-plus fa-fw mr-2"></i>New Request</span>
                 </a>
             </div>
             <!-- Separator with title -->
@@ -54,17 +54,19 @@
             <a href="#" class="bg-dark list-group-item list-group-item-action">
                 <div class="d-flex w-100 justify-content-start align-items-center">
                     <i class="fas fa-lg fa-user fa-fw mr-3"></i>
-                    <span class="menu-collapsed">Profile</span>
+                        <span class="menu-collapsed">Profile</span>
                 </div>
             </a>
             <a href="#" class="bg-dark list-group-item list-group-item-action">
                 <div class="d-flex w-100 justify-content-start align-items-center">
                     <i class="fas fa-lg fa-bell fa-fw mr-3"></i>
-                    <span class="menu-collapsed">Notifications <span class="badge badge-pill badge-primary ml-2">5</span></span>
+                    <span class="menu-collapsed">Notifications <span class="badge badge-pill badge-success ml-2">5</span></span>
                 </div>
             </a>
             <!-- Separator without title -->
-            <li class="list-group-item sidebar-separator menu-collapsed"></li>
+            <li class="list-group-item sidebar-separator-title text-muted d-flex align-items-center menu-collapsed">
+
+            </li>
             <!-- /END Separator -->
             <a href="#" class="bg-dark list-group-item list-group-item-action">
                 <div class="d-flex w-100 justify-content-start align-items-center">
