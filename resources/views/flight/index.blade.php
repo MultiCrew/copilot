463d26df2aa7572730ccee6dc4a7b2db79f1bce8
Calum Shepherd <calum.shep@icloud.com>
2019-12-16 21:25:51 +0000

    added loading spinner to table

--- resources/views/flight/index.blade.php
+++ resources/views/flight/index.blade.php
@@ -4,7 +4,7 @@
 <div class="form-group">
 	<input type="text" name="search" id="search" class="form-control" placeholder="Search Flights" />
 </div>
-	<table class="table">
+	<table class="table table-striped table-hover">
 		<thead>
 			<tr>
 				<th>#</th>
@@ -15,34 +15,45 @@
 			</tr>
 		</thead>
 		<tbody>
+            <tr id="loading-icon">
+                <td colspan="5">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <span class="spinner-border mr-3 text-warning" role="status"></span>
+                        <span>Loading...</span>
+                    </div>
+                </td>
+            </tr>
 		</tbody>
 	</table>
 @endsection
 
 @section('scripts')
 <script>
-	$(document).ready(function(){
-	
-	 fetch_customer_data();
-	
-	 function fetch_customer_data(query = '')
-	 {
-	  $.ajax({
-	   url:"{{ route('flights.search') }}",
-	   method:'GET',
-	   data:{query:query},
-	   dataType:'json',
-	   success:function(data)
-	   {
-		$('tbody').html(data.table_data);
-	   }
-	  })
-	 }
-	
-	 $(document).on('keyup', '#search', function(){
-	  var query = $(this).val();
-	  fetch_customer_data(query);
-	 });
+	$(document).ready(function()
+    {
+        function fetch_customer_data(query = '')
+        {
+            $.ajax({
+                url: "{{ route('flights.search') }}",
+                method: 'GET',
+                data: {query:query},
+                dataType: 'json',
+                success: function(data)
+                {
+                    $('#loading-icon').remove();
+                    $('tbody').attr('class', '');
+                    $('tbody').html(data.table_data);
+                }
+            })
+        }
+
+        fetch_customer_data();
+
+        $(document).on('keyup', '#search', function()
+        {
+            var query = $(this).val();
+            fetch_customer_data(query);
+        });
 	});
-	</script>
+</script>
 @endsection
