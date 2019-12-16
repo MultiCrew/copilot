5ebc2dfd7b8540f6bd3310fbc50caebd38da5ec8
Calum Shepherd <calum.shep@icloud.com>
2019-12-16 21:29:02 +0000

    reformatted

--- resources/views/flight/index.blade.php
+++ resources/views/flight/index.blade.php
@@ -1,33 +1,37 @@
 @extends('layouts.base')
 
 @section('content')
+
 <div class="form-group">
-	<input type="text" name="search" id="search" class="form-control" placeholder="Search Flights" />
+	<input type="text" name="search" id="search" class="form-control" placeholder="Search Flights">
 </div>
-	<table class="table table-striped table-hover">
-		<thead>
-			<tr>
-				<th>#</th>
-				<th>Departure</th>
-				<th>Arrival</th>
-				<th>Aircraft</th>
-				<th>Join</th>
-			</tr>
-		</thead>
-		<tbody>
-            <tr id="loading-icon">
-                <td colspan="5">
-                    <div class="d-flex align-items-center justify-content-center">
-                        <span class="spinner-border mr-3 text-warning" role="status"></span>
-                        <span>Loading...</span>
-                    </div>
-                </td>
-            </tr>
-		</tbody>
-	</table>
+
+<table class="table table-striped table-hover">
+	<thead>
+		<tr>
+			<th>#</th>
+			<th>Departure</th>
+			<th>Arrival</th>
+			<th>Aircraft</th>
+			<th>Join</th>
+		</tr>
+	</thead>
+	<tbody>
+        <tr id="loading-icon">
+            <td colspan="5">
+                <div class="d-flex align-items-center justify-content-center">
+                    <span class="spinner-border mr-3 text-warning" role="status"></span>
+                    <span>Loading...</span>
+                </div>
+            </td>
+        </tr>
+	</tbody>
+</table>
+
 @endsection
 
 @section('scripts')
+
 <script>
 	$(document).ready(function()
     {
@@ -40,7 +44,7 @@
                 dataType: 'json',
                 success: function(data)
                 {
-                    $('#loading-icon').remove();
+                    $('#loading-icon').hide();
                     $('tbody').attr('class', '');
                     $('tbody').html(data.table_data);
                 }
@@ -51,9 +55,11 @@
 
         $(document).on('keyup', '#search', function()
         {
+            $('#loading-icon').show();
             var query = $(this).val();
             fetch_customer_data(query);
         });
 	});
 </script>
+
 @endsection
