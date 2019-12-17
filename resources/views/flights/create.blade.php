a4bbd51bd7e6d3b28a82b4ba400951c4a1244813
Calum Shepherd <calum.shep@icloud.com>
2019-12-17 11:39:52 +0000

    added csrf field

--- resources/views/flights/create.blade.php
+++ resources/views/flights/create.blade.php
@@ -2,26 +2,27 @@
 
 @section('content')
 
-<form method="post" action="/flights">
-    <div class="form-row">
-        <div class="form-group col-md-6">
-            <label for="departure">Departure</label>
-            <input type="text" name="departure" id="departure" class="form-control">
+    <form method="post" action="/flights">
+        @csrf
+
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="departure">Departure</label>
+                <input type="text" name="departure" id="departure" class="form-control">
+            </div>
+
+            <div class="form-group col-md-6">
+                <label for="arrival">Arrival</label>
+                <input type="text" name="arrival" id="arrival" class="form-control">
+            </div>
         </div>
 
-        <div class="form-group col-md-6">
-            <label for="arrival">Arrival</label>
-            <input type="text" name="arrival" id="arrival" class="form-control">
+        <div class="form-group">
+            <label for="aircraft">Aircraft</label>
+            <input type="text" name="aircraft" id="aircraft" class="form-control">
         </div>
-    </div>
 
-    <div class="form-group">
-        <label for="aircraft">Aircraft</label>
-        <input type="text" name="aircraft" id="aircraft" class="form-control">
-    </div>
-
-    <button type="submit" class="btn btn-primary">Add <i class="fas fa-plus fa-fw ml-2"></i></button>
-
-</form>
+        <button type="submit" class="btn btn-primary">Add <i class="fas fa-plus fa-fw ml-2"></i></button>
+    </form>
 
 @endsection
