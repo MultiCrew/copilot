026685b3e751880170627b525bbc8e86323af54d
Calum Shepherd <calum.shep@icloud.com>
2019-12-25 12:19:05 +0000

    fixed table column widths

--- resources/views/flights/index.blade.php
+++ resources/views/flights/index.blade.php
@@ -4,37 +4,40 @@
 
 @if(isset($acceptedRequests))
 
-    <div class="card">
+    <div class="card mb-4">
         <div class="card-body">
             <h5 class="card-title">Accepted Flights</h5>
 
             <table class="table table-hover card-text border">
                 <thead class="thead-light">
-                    <tr>
-                        <th>Acceptee</th>
-                        <th>Departure</th>
-                        <th>Arrival</th>
-                        <th>Aircraft</th>
-                        <th></th>
+                    <tr class="d-flex">
+                        <th class="col-2">Acceptee</th>
+                        <th class="col-3">Departure</th>
+                        <th class="col-3">Arrival</th>
+                        <th class="col-2">Aircraft</th>
+                        <th class="col-2"></th>
                     </tr>
                 </thead>
 
                 <tbody>
                     @foreach($acceptedRequests as $flight)
-                        <tr>
-                            <td>
+                        <tr class="d-flex">
+                            <td class="col-2">
                                 @if($flight->acceptee_id == \Auth::user()->id)
                                     You!
                                 @else
                                     {{ App\Models\Users\User::find($flight->acceptee_id)->username }}
                                 @endif
                             </td>
-                            <td>{{ $flight->departure }}</td>
-                            <td>{{ $flight->arrival }}</td>
-                            <td>{{ $flight->aircraft }}</td>
-                            <td class="p-0">
-                                <a href="{{ route('dispatch.plan', $flight->id) }}" class="btn btn-sm m-2 btn-success">
-                                    Plan &raquo;
+                            <td class="col-3">{{ $flight->departure }}</td>
+                            <td class="col-3">{{ $flight->arrival }}</td>
+                            <td class="col-2">{{ $flight->aircraft }}</td>
+                            <td class="py-0 col-2 text-right">
+                                <a href="{{ route('flights.show', [$flight->id]) }}" class="btn btn-sm my-2 btn-info">
+                                    View<i class="fas fa-fw ml-2 fa-search"></i>
+                                </a>
+                                <a href="{{ route('dispatch.plan', [$flight->id]) }}" class="btn btn-sm my-2 btn-success">
+                                    Plan<i class="fas fa-fw ml-2 fa-angle-double-right"></i>
                                 </a>
                             </td>
                         </tr>
@@ -71,23 +74,23 @@
 
         <table class="table table-hover card-text border">
             <thead class="thead-light">
-                <tr>
-                    <th>User</th>
-                    <th>Departure</th>
-                    <th>Arrival</th>
-                    <th>Aircraft</th>
-                    <th></th>
+                <tr class="d-flex">
+                    <th class="col-2">User</th>
+                    <th class="col-3">Departure</th>
+                    <th class="col-3">Arrival</th>
+                    <th class="col-2">Aircraft</th>
+                    <th class="col-2"></th>
                 </tr>
             </thead>
 
             <tbody>
                 @foreach ($flights as $flight)
-                    <tr>
-                        <td>{{ App\Models\Users\User::find($flight->requestee_id)->username }}</td>
-                        <td>{{ $flight->departure }}</td>
-                        <td>{{ $flight->arrival }}</td>
-                        <td>{{ $flight->aircraft }}</td>
-                        <td class="p-0">
+                    <tr class="d-flex">
+                        <td  class="col-2">{{ App\Models\Users\User::find($flight->requestee_id)->username }}</td>
+                        <td  class="col-3">{{ $flight->departure }}</td>
+                        <td  class="col-3">{{ $flight->arrival }}</td>
+                        <td  class="col-2">{{ $flight->aircraft }}</td>
+                        <td class="p-0 col-2 text-right">
                             <a
                             href="{{route('flights.accept', ['id' => $flight->id])}}"
                             class="btn btn-sm m-2 btn-success">
