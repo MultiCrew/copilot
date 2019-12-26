fc05cec49ca58c593a7ab51ad3bb60fa0d7bf83a
Calum Shepherd <calum.shep@icloud.com>
2019-12-26 16:43:20 +0000

    initial revision of FlightPlan class and table migration

--- app/Models/Flights/FlightPlan.php
+++ app/Models/Flights/FlightPlan.php
@@ -6,5 +6,52 @@ use Illuminate\Database\Eloquent\Model;
 
 class FlightPlan extends Model
 {
-    //
+    /**
+     * Attributes that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ofp_json'
+    ];
+
+    /**
+     * Gets the flight associated with the plan
+     *
+     * @return     Flight   The flight which references this plan
+     */
+    public function getFlight()
+    {
+        return Flight::where('plan_id', $this->id)->firstOrFail();
+    }
+
+    /**
+     * Accepts the flight plan as the logged in user
+     */
+    public function accept()
+    {
+        $flight = $this->getFlight();
+
+        if (Auth::user()->id === $flight->requestee)
+            $this->requestee_accept = 1;
+        else
+            $this->accepttee_accept = 1;
+
+        $this->save();
+    }
+
+    /**
+     * Rejects the flight plan as the logged in user
+     */
+    public function reject()
+    {
+        $flight = $this->getFlight();
+
+        if (Auth::user()->id === $flight->requestee)
+            $this->requestee_accept = 0;
+        else
+            $this->accepttee_accept = 0;
+
+        $this->save();
+    }
 }
