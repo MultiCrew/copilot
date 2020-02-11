da2059c71ed7df3b0d92707cbf11de0654e10060
Harry Cameron <hcphoon01@gmail.com>
2020-02-11 13:24:56 +0000

    changed user notification table migration

--- app/Models/Users/UserNotifications.php
+++ app/Models/Users/UserNotifications.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserNotifications extends Model
 {
-    //
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'airport_request' => 'array',
+        'aircraft_request' => 'array',
+    ];
 }
