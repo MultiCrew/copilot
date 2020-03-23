be0295233ce6149b7639e78751c625b4bb600e64
Calum Shepherd <calum.shep@icloud.com>
2020-03-23 19:49:57 +0000

    added roles output to account page

--- resources/views/auth/account.blade.php
+++ resources/views/auth/account.blade.php
@@ -33,20 +33,33 @@
         </div>
     </div>
 
-    <div class="form-group">
-        <label for="email">Email Address</label>
-        <input
-        type="email"
-        id="email"
-        name="email"
-        class="form-control"
-        value="{{ Auth::user()->email }}"
-        autocomplete="email">
+    <div class="form-group form-row">
+        <div class="col-md-6">
+            <label for="email">Email Address</label>
+            <input
+            type="email"
+            id="email"
+            name="email"
+            class="form-control"
+            value="{{ Auth::user()->email }}"
+            autocomplete="email">
+        </div>
+        <div class="col-md-6">
+            <label for="username">Role(s)</label>
+            <input
+            type="text"
+            id="username"
+            name="username"
+            class="form-control"
+            readonly
+            value="@foreach(Auth::user()->roles as $role) {{ $role->name.' ' }} @endforeach"
+            autocomplete="username">
+        </div>
     </div>
 
     <div class="form-row">
         <div class="form-group col-md-6">
-            <label for="password">Password</label>
+            <label for="password">New Password</label>
             <input
             type="password"
             id="password"
@@ -70,6 +83,21 @@
     <button type="submit" class="btn btn-success">
         <i class="fas fa-fw fa-save mr-3"></i>Save
     </button>
+
+    @cannot('search')
+        <hr>
+        <h3>Beta Application</h3>
+        <p class="lead">Join the Copilot beta testing program now!</p>
+        <p>
+            Did you know you are eligible to apply for the beta testing program
+            for Copilot? You'll be part of the team testing new features and
+            generating new ideas. Interested?
+        </p>
+        <a class="btn btn-success btn-lg"
+        href="{{ route('account.apply') }}">
+            Apply Now<i class="fas fa-angle-double-right ml-2"></i>
+        </a>
+    @endcan
 </form>
 
 @endsection
