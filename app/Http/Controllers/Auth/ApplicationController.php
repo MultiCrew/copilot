5fe354b32fe82e61ec60e2d643a829cc2c70535d
Harry Cameron <hcphoon01@gmail.com>
2020-03-24 22:20:41 +0000

    added middleware to application controller

--- app/Http/Controllers/Auth/ApplicationController.php
+++ app/Http/Controllers/Auth/ApplicationController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware(['role:new']);
+    }
+
     /**
      * Display a listing of the resource.
      *
