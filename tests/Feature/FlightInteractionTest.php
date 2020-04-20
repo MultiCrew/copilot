2afb2228992cc888611d7ce1acc23a6c53eea5a5
Harry Cameron <hcphoon01@gmail.com>
2020-04-20 22:38:53 +0100

    fixed issues with $this

--- tests/Feature/FlightInteractionTest.php
+++ tests/Feature/FlightInteractionTest.php
@@ -2,28 +2,28 @@
 
 namespace Tests\Feature;
 
-use App\Models\Flights\Flight;
-use App\Models\Users\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Users\User;
+use App\Models\Flights\Flight;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class FlightInteractionTest extends TestCase
 {
     protected $flight;
     protected $user1, $user2;
 
-    public function setUp(): void
+	public function setUp(): void
     {
-        $this->user1 = factory(User::class)->make();
-        $this->user2 = factory(User::class)->make();
+        $this->user1 = factory(User::class)->create();
+        $this->user2 = factory(User::class)->create();
 
         $this->flight = Flight::create([
             'departure'     => 'EGPD',
             'arrival'       => 'EHAM',
             'aircraft'      => 'B738',
             'public'        => 1,
-            'requestee_id'  => $user1->id
+            'requestee_id'  => $this->user1->id
         ]);
     }
 
@@ -36,8 +36,8 @@ class FlightInteractionTest extends TestCase
     {
         $this->assertEquals($this->user1, $this->flight->requestee);
         $this->assertTrue($this->flight->isRequestee($this->user1));
-        $this->assertTrue($this->flight->isInvolved($user1));
-        $this->assertFalse($this->flight->isInvolved($user2));
+        $this->assertTrue($this->flight->isInvolved($this->user1));
+        $this->assertFalse($this->flight->isInvolved($this->user2));
         $this->assertEquals(null, $this->flight->acceptee);
     }
 
@@ -48,12 +48,12 @@ class FlightInteractionTest extends TestCase
      */
     public function testFlightAccept()
     {
-        $this->actingAs($user2);
+        $this->actingAs($this->user2);
         $this->flight->accept();
 
         $this->assertEquals($this->user2, $this->flight->acceptee);
         $this->assertTrue($this->flight->isAcceptee($this->user2));
-        $this->assertTrue($this->flight->isInvolved($user2));
+        $this->assertTrue($this->flight->isInvolved($this->user2));
         $this->assertTrue($this->flight->isAccepted());
     }
 
