f78261843d213e9d48b8991e00fccd6b66488373
Calum Shepherd <calum.shep@icloud.com>
2020-04-20 14:32:16 +0100

    added initial flight unit test

---
+++ tests/Unit/Flights/FlightTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit\Flights;
+
+use App\Models\Flights\Flight;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class php extends TestCase
+{
+    protected $flight;
+
+    public function setUp(): void
+    {
+        $this->flight = new Flight();
+        $this->flight->fill([
+            'departure' => 'EGPD',
+            'arrival'   => 'EHAM',
+            'aircraft'  => 'B738'
+        ]);
+    }
+
+    /**
+     * Tests that Flight has been created correctly
+     *
+     * @return void
+     */
+    public function testflightCreationTest()
+    {
+        $this->assertEquals('EGPD', $this->flight->departure);
+        $this->assertEquals('EHAM', $this->flight->arrival);
+        $this->assertEquals('B738', $this->flight->aircraft);
+    }
+
+    public function tearDown(): void
+    {
+        $this->flight = null;
+    }
+}
