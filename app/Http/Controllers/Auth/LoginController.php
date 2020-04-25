b09640acbfe128fe598d5efc96f0b620ea695dd0
Harry Cameron <hcphoon01@gmail.com>
2020-04-25 10:13:32 +0100

    implemented username login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -50,4 +50,44 @@ class LoginController extends Controller
 
         return redirect('/flights');
     }
+
+    /**
+    * Get the login username to be used by the controller.
+    *
+    * @return string
+    */
+    public function username()
+    {
+        $login = request()->input('identity');
+
+        $field = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        request()->merge([$field => $login]);
+
+        return $field;
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $messages = [
+            'identity.required' => 'Email or username cannot be empty',
+            'email.exists' => 'Email or username already registered',
+            'username.exists' => 'Username is already registered',
+            'password.required' => 'Password cannot be empty',
+        ];
+
+        $request->validate([
+            'identity' => 'required|string',
+            'password' => 'required|string',
+            'email' => 'string|exists:users',
+            'username' => 'string|exists:users',
+        ], $messages);
+    }
 }
