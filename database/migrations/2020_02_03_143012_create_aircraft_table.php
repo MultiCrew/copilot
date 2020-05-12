8fb14e32faf48880bb7a83b18b4ee07ea865e29e
Harry Cameron <hcphoon01@gmail.com>
2020-05-12 18:52:36 +0100

    migrated airport and aircraft database updates to laravel excel

--- database/migrations/2020_02_03_143012_create_aircraft_table.php
+++ database/migrations/2020_02_03_143012_create_aircraft_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAircraftsTable extends Migration
+class CreateAircraftTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAircraftsTable extends Migration
      */
     public function up()
     {
-        Schema::create('aircrafts', function (Blueprint $table) {
+        Schema::create('aircraft', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('icao');
             $table->string('iata');
