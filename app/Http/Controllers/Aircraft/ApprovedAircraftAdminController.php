bea99d1886a6f4a011caf8bf0a523c4343b16c37
Calum Shepherd <calum.shep@icloud.com>
2020-06-14 11:25:01 +0100

    added admin controller

---
+++ app/Http/Controllers/Aircraft/ApprovedAircraftAdminController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Aircraft;
+
+use App\Http\Controllers\Controller;
+use App\Http\Models\Aircraft\ApprovedAircraft;
+use Illuminate\Http\Request;
+
+class ApprovedAircraftAdminController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $aircraft = ApprovedAircraft()::create([
+            'icao' => $request->icao,
+            'name' => $request->name,
+            'sim'  => $request->sim
+        ]);
+
+        return redirect()->route('aircraft.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
