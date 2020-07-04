dcd63c842842fcbf57afb046d400963623c87e35
Calum Shepherd <calum.shep@icloud.com>
2020-07-04 12:38:45 +0100

    fixed fillable aircraft id

--- app/Models/Flights/FlightRequest.php
+++ app/Models/Flights/FlightRequest.php
@@ -30,7 +30,7 @@ class FlightRequest extends Model
      * @var array
      */
     protected $fillable = [
-        'departure', 'arrival', 'aircraft', 'requestee_id', 'acceptee_id', 'public'
+        'departure', 'arrival', 'aircraft_id', 'requestee_id', 'acceptee_id', 'public'
     ];
 
     /**
