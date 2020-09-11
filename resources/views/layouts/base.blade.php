19410135a44bf2ff88f9c03f82f9cdfcd84e8ed5
Calum Shepherd <calum.shep@icloud.com>
2020-09-11 13:22:01 +0100

    added changelog modal and button

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -141,6 +141,29 @@
         </div>
     </div>
 
+    <!-- Modal -->
+    <div class="modal fade" id="versionModal" tabindex="-1" aria-labelledby="versionModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="versionModalLabel">Changelog</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                    @include('includes.changelog')
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     @stack('append-scripts')
 </body>
 </html>
