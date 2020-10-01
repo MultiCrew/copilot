eaddb0a78316fc8d30edba288eb430d2eab8de74
Harry Cameron <hcphoon01@gmail.com>
2020-10-01 22:58:05 +0100

    added log class to discord controller

--- app/Http/Controllers/Discord/DiscordController.php
+++ app/Http/Controllers/Discord/DiscordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Discord;
 
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Users\UserNotification;
@@ -65,6 +66,7 @@ class DiscordController extends Controller
 		
 				// Failed to get user details
 				Log::error($e);
+				exit('Oops');
 		
 			}
 		}
