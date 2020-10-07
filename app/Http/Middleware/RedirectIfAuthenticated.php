7df831c85af80bc1d13069846d023a6a4d92a1f3
Harry Cameron <hcphoon01@gmail.com>
2020-10-07 17:59:19 +0100

    fixed spelling of intended

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect()->indented('/flights');
+            return redirect()->intended('/flights');
         }
 
         return $next($request);
