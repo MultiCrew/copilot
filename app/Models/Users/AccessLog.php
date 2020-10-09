f4911a5ce4d362b2d51b2cecc356f1fc19457088
Harry Cameron <hcphoon01@gmail.com>
2020-10-09 14:28:25 +0100

    added middleware to log client ip on every authorized route

---
+++ app/Models/Users/AccessLog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Users;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AccessLog extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'ip',
+    ];
+}
