341c25b790aebdb078a9a5d77e094dd4e3d17644
Harry Cameron <hcphoon01@gmail.com>
2020-10-11 10:04:07 +0100

    created request resource

---
+++ app/Http/Resources/Flights/RequestResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\Flights;
+
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RequestResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'plan_id' => $this->plan_id,
+            'code' => $this->code,
+            'public' => $this->public,
+            'departure' => $this->departure,
+            'arrival' => $this->arrival,
+            'created_at' => (string) $this->created_at,
+            'expiry' => $this->expiry,
+            'aircraft' => new AircraftResource($this->aircraft),
+            'requestee' => new UserResource($this->requestee),
+            'acceptee' => new UserResource($this->acceptee)
+        ];
+    }
+}
