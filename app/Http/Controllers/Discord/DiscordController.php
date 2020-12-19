5e12886aecfb011aa974512009be64eccabcd229
Harry Cameron <hcphoon01@gmail.com>
2020-12-19 16:04:40 +0000

    feat: migrated discord controller to socialite

--- app/Http/Controllers/Discord/DiscordController.php
+++ app/Http/Controllers/Discord/DiscordController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Users\UserNotification;
 use App\Notifications\DiscordSendData;
+use Laravel\Socialite\Facades\Socialite;
 use Wohali\OAuth2\Client\Provider\Discord;
 
 class DiscordController extends Controller
@@ -16,78 +17,57 @@ class DiscordController extends Controller
     public function __construct()
     {
         $this->middleware(['auth']);
-	}
-	
-	/**
-     * Connect a Discord account.
+    }
+
+    /**
+     * Redirect the user to the Discord authentication page.
      *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirect()
+    {
+        return Socialite::driver('discord')->scopes(['guilds.join', 'identify'])->redirect();
+    }
+
+    /**
+     * Obtain the user information from Discord.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function callback()
+    {
+        $discordUser = Socialite::driver('discord')->user();
+
+        $user = Auth::user();
+        $user->discord_id = $discordUser->getId();
+        $user->save();
+        $message = $user->username . ', this is a confirmation that your MultiCrew account is now connected to Discord.';
+        $roleList = $user->roles;
+        $roles = array();
+        for ($i = 0; $i < count($roleList); $i++) {
+            $role = $roleList[$i];
+            array_push($roles, $role->discord_id);
+        }
+        $user->notify(new DiscordSendData('connection', $message, $user, $roles));
+        return redirect()->route('account.index');
+    }
+
+    /**
+     * Disconnect a Discord Account
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function connect(Request $request) {
-		$provider = new Discord([
-			'clientId' => config('services.discord.id'),
-    		'clientSecret' => config('services.discord.secret'),
-    		'redirectUri' => config('services.discord.redirect')
-		]);
-		$options = [
-			'scope' => 'identify'
-		];
-		if (!isset($_GET['code'])) {
-			// Get authorization code
-			$authUrl = $provider->getAuthorizationUrl($options);
-			header('Location: ' . $authUrl); exit;
-		
-		} else {
-		
-			//  Get an access token using the provided authorization code
-			$token = $provider->getAccessToken('authorization_code', [
-				'code' => $_GET['code']
-			]);
-		
-			// Get id and save it to the database
-			try {
-		
-				$discordUser = $provider->getResourceOwner($token);
-				$user = Auth::user();
-				$user->discord_id = $discordUser->getId();
-				$user->save();
-				$message = $user->username . ', this is a confirmation that your MultiCrew account is now connected to Discord.';
-				$roleList = $user->roles;
-				$roles = array();
-				for ($i=0; $i < count($roleList); $i++) { 
-					$role = $roleList[$i];
-					array_push($roles, $role->discord_id);
-				}
-				$user->notify(new DiscordSendData('connection', $message, $user, $roles));
-				return redirect()->route('account.index');
-		
-			} catch (Exception $e) {
-		
-				// Failed to get user details
-				Log::error($e);
-				exit('Oops');
-		
-			}
-		}
-	}
+    public function disconnect(Request $request)
+    {
+        $user = Auth::user();
+        $user->discord_id = null;
+        $user->save();
 
-	/**
-	 * Disconnect a Discord Account
-	 * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-	 */
-	public function disconnect(Request $request)
-	{
-		$user = Auth::user();
-		$user->discord_id = null;
-		$user->save();
+        $userNotification = UserNotification::where('user_id', Auth::id())->first();
+        $userNotification->request_accepted_push = 0;
+        $userNotification->plan_reviewed_push = 0;
+        $userNotification->save();
 
-		$userNotification = UserNotification::where('user_id', Auth::id())->first();
-		$userNotification->request_accepted_push = 0;
-		$userNotification->plan_reviewed_push = 0;
-		$userNotification->save();
-
-		return redirect()->route('account.index');
-	}
+        return redirect()->route('account.index');
+    }
 }
