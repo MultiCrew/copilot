16b29d358438b61a8410ff25b56ea4b2d7f75a72
Harry Cameron <hcphoon01@gmail.com>
2020-12-19 16:29:30 +0000

    feat: add user to discord server automatically
    User is added if they are not already in the server when connecting their discord account, they are
    also assigned the correct roles
    
    fix #150

--- app/Http/Controllers/Discord/DiscordController.php
+++ app/Http/Controllers/Discord/DiscordController.php
@@ -26,7 +26,7 @@ class DiscordController extends Controller
      */
     public function redirect()
     {
-        return Socialite::driver('discord')->scopes(['guilds.join', 'identify'])->redirect();
+        return Socialite::driver('discord')->scopes(['guilds.join', 'guilds'])->redirect();
     }
 
     /**
@@ -38,16 +38,32 @@ class DiscordController extends Controller
     {
         $discordUser = Socialite::driver('discord')->user();
 
+        $discordId = $discordUser->getId();
+
         $user = Auth::user();
-        $user->discord_id = $discordUser->getId();
-        $user->save();
-        $message = $user->username . ', this is a confirmation that your MultiCrew account is now connected to Discord.';
         $roleList = $user->roles;
         $roles = array();
         for ($i = 0; $i < count($roleList); $i++) {
             $role = $roleList[$i];
             array_push($roles, $role->discord_id);
         }
+
+        $token = $discordUser->token;
+        $endpoint = config('services.discord.base_url') . '/guilds/' . config('services.discord.guild') . '/members/' . $discordId;
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('PUT', $endpoint, [
+            'json' => [
+                'access_token' => $token,
+                'roles' => $roles
+            ],
+            'headers' => [
+                'Authorization' => 'Bot ' . config('services.discord.bot_token'),
+            ]
+        ]);
+
+        $user->discord_id = $discordId;
+        $user->save();
+        $message = $user->username . ', this is a confirmation that your MultiCrew account is now connected to Discord.';
         $user->notify(new DiscordSendData('connection', $message, $user, $roles));
         return redirect()->route('account.index');
     }
