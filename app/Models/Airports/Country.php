e12aad6999accd0a93290bf9f5fd1a716c7905aa
Harry Cameron <hcphoon01@gmail.com>
2020-12-22 10:57:54 +0000

    feat: add continent conversion to country model

--- app/Models/Airports/Country.php
+++ app/Models/Airports/Country.php
@@ -14,4 +14,34 @@ class Country extends Model
     protected $fillable = [
         'code', 'name', 'continent',
     ];
+
+    public function getContinentAttribute($value)
+    {
+        switch ($value) {
+            case 'AF':
+                return 'Africa';
+                break;
+            case 'AN':
+                return 'Antartica';
+                break;
+            case 'AS':
+                return 'Asia';
+                break;
+            case 'EU':
+                return 'Europe';
+                break;
+            case 'NA':
+                return 'North America';
+                break;
+            case 'OC':
+                return 'Oceania';
+                break;
+            case 'SA':
+                return 'South America';
+                break;
+            default:
+                return null;
+                break;
+        }
+    }
 }
