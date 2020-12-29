2149dcf373f878a55d5b3da5b8dacdc50e7d9d6c
Calum Shepherd <calum.shep@icloud.com>
2020-12-29 13:45:10 +0000

    Add ability for user to edit own pending fleet additions
    - Also allow user to delete own pending fleet additions
    - Refactor approved aircraft controllers into single controller class
    - Add logic to check user roles and permissions on models
    - Update views to match new backend logic

--- app/Http/Controllers/Aircraft/ApprovedAircraftController.php
+++ app/Http/Controllers/Aircraft/ApprovedAircraftController.php
@@ -6,13 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Models\Aircraft\ApprovedAircraft;
 use App\Models\FlightSim\Simulator;
 use Illuminate\Http\Request;
+use App\Models\Aircraft\Aircraft;
+use Auth;
 
 class ApprovedAircraftController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Resource index
      */
     public function index()
     {
@@ -40,10 +40,77 @@ class ApprovedAircraftController extends Controller
             'icao' => $request->icao,
             'name' => $request->name,
             'sim'  => $request->sim,
-            'added_by' => \Auth::user()->username,
+            'added_by' => Auth::user()->username,
             'approved' => false
         ]);
 
+        return redirect()->route('aircraft.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  ApprovedAircraft $aircraft
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ApprovedAircraft $aircraft)
+    {
+        if (!($aircraft->added_by === Auth::user()->username || Auth::user()->hasRole('admin'))) {
+            return redirect()->route('aircraft.index');
+        }
+
+        return view('aircraft.show', [
+            'aircraft'      => $aircraft,
+            'simulators'    => Simulator::all(),
+            'icao_acft'     => Aircraft::where('icao', $aircraft->icao)->get()->first(),
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  ApprovedAircraft $aircraft
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ApprovedAircraft $aircraft, Request $request)
+    {
+        if (!($aircraft->added_by === Auth::user()->username || Auth::user()->hasRole('admin'))) {
+            return redirect()->route('aircraft.index');
+        }
+
+        $request->validate([
+            'icao' => 'required|exists:aircraft,icao',
+            'name' => 'required|max:100',
+            'sim' => 'required|max:50'
+        ]);
+
+        $aircraft->icao = $request->icao;
+        $aircraft->name = $request->name;
+        $aircraft->sim = $request->sim;
+
+        if ($request->action == 'approve' && Auth::user()->hasRole('admin')) {
+            $aircraft->approved = true;
+        }
+
+        $aircraft->save();
+
+        return redirect()->route('aircraft.show', $aircraft);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  ApprovedAircraft $aircraft
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ApprovedAircraft $aircraft)
+    {
+        if (!($aircraft->added_by === Auth::user()->username || Auth::user()->hasRole('admin'))) {
+            return redirect()->route('aircraft.index');
+        }
+
+        $aircraft->delete();
         return redirect()->route('aircraft.index');
     }
 }
